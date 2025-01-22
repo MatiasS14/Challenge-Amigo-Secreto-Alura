677ee26d6676b9fc2672497f85199c99bb365704
MatiasS14 <matias.solanes14@gmail.com>
2025-01-22 16:28:35 -0300

    Funcionalidad ingresar amigo nuevo, con validacion agregados

--- challenge-amigo-secreto_esp-main/app.js
+++ challenge-amigo-secreto_esp-main/app.js
@@ -1,1 +1,35 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let listaAmigos = [];
+let amigoSeleccionado = "";
+
+
+function agregarAmigo(){
+    let amigoIngresado = document.getElementById('amigo').value;
+
+    if(validarAmigo(amigoIngresado)){
+        listaAmigos.push(amigoIngresado);
+        document.getElementById('amigo').value = '';
+        actualizarLista();
+    }else{
+        alert('Debe ingresar un nombre valido');
+    }
+}
+
+function validarAmigo(amigo){
+    //la funcion trim elimina los espacio vacios
+    //lo que evita que se agreguen strings vacios
+    return amigo.trim().length > 0;
+}
+
+function actualizarLista(){
+    let listaHTML = document.getElementById('listaAmigos'); 
+    listaHTML.innerHTML='';
+    listaAmigos.forEach(amigo => {
+        //crea un nodo li para agregar en html
+        var node = document.createElement('li');
+        //le agrego el string al nodo
+        node.appendChild(document.createTextNode(amigo));
+        //agrego el nodo li al la lista ul en el html
+        listaHTML.appendChild(node);
+    });
+
+}
